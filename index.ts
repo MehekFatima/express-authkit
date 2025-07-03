73af498d1fed61d141ddb6c5d3de87230a2759bf
mehek <mehekf18@gmail.com>
2025-07-03 16:54:38 +0530

    chore: release v1.0.2 with README improvements and export fixes

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 export { TokenManager } from './src/token/tokenManager';
 export { authMiddleware } from './src/middleware/authMiddleware';
+export { protectMiddleware } from './src/middleware/protectMiddleware';
 export { RedisStore } from './src/session/redisStore';
 
 export * from './src/token/types';
